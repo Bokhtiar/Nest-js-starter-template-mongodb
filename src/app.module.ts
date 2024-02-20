2f7a43afa43dcadcdfb99563131963e2618c231b
bokhtiar <bokhtiartoshar1@gmail.com>
2024-02-20 14:45:32 +0600

    category g module service controdller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { NotesModule } from './notes/notes.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://bokhtiar:bokhtiar@cluster0.8z6ss.mongodb.net/nestjs?retryWrites=true&w=majority'),
-    NotesModule
+    NotesModule,
+    CategoryModule
   ],
   controllers: [AppController],
   providers: [AppService],
