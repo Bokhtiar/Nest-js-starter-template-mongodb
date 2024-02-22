6ad8aa525527fb871378d22b9e6b8c3c0d39e4dd
bokhtiar <bokhtiartoshar1@gmail.com>
2024-02-22 16:04:16 +0600

    cart module service controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { NotesModule } from './notes/notes.module';
 import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
+import { CartModule } from './cart/cart.module';
 
 
 @Module({
@@ -12,7 +13,8 @@ import { ProductModule } from './product/product.module';
     MongooseModule.forRoot('mongodb+srv://bokhtiar:bokhtiar@cluster0.8z6ss.mongodb.net/nestjs?retryWrites=true&w=majority'),
     NotesModule,
     CategoryModule,
-    ProductModule
+    ProductModule,
+    CartModule
   ],
   controllers: [AppController],
   providers: [AppService],
